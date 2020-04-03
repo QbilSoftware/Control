6bf4b88f3d5e5c33505df6ae67b5cd0a15c72278
Faizan Akram <hello@faizanakram.me>
2020-04-03 17:58:54 +0530

    improves FTP server
    Signed-off-by: Faizan Akram <hello@faizanakram.me>

---
+++ src/FtpException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Qbil\Control;
+
+class FtpException extends \RuntimeException
+{
+}
--- src/FtpServer.php
+++ src/FtpServer.php
@@ -4,22 +4,49 @@ namespace Qbil\Control;
 
 class FtpServer
 {
+    /**
+     * @var resource
+     */
     private $conn;
+    /**
+     * @var string
+     */
+    private $host;
+    /**
+     * @var string
+     */
+    private $username;
+    /**
+     * @var string
+     */
+    private $password;
+    /**
+     * @var bool
+     */
+    private $passive;
+    /**
+     * @var int
+     */
+    private $timeout;
 
-    public function __construct($host, $username, $password, $passive = true)
+    public function __construct(string $host, string $username, string $password, bool $passive = true, int $timeout = 30)
     {
-        $this->conn = ftp_connect($host, null, 30);
-        if (!$this->conn || !ftp_login($this->conn, $username, $password)) {
-            throw new \Exception('Could not login or connect to FTP');
-        }
-        ftp_pasv($this->conn, $passive);
+        $this->host = $host;
+        $this->username = $username;
+        $this->password = $password;
+        $this->passive = $passive;
+        $this->timeout = $timeout;
     }
 
-    public function listFilesMatching($directory, $regex)
+    /**
+     * @throws FtpException
+     */
+    public function listFilesMatching(string $directory, string $regex): array
     {
+        $this->connect();
         $files = @ftp_nlist($this->conn, $directory);
-        if (!$files) {
-            throw new \Exception('Could not retrieve list of files.');
+        if (false === $files) {
+            throw new FtpException('Could not retrieve list of files.');
         }
         $matchingFiles = [];
         foreach ($files as $file) {
@@ -28,12 +55,38 @@ class FtpServer
             }
             $matchingFiles[] = $matches[1];
         }
+        $this->close();
 
         return $matchingFiles;
     }
 
-    public function downloadFile($localPath, $remotePath)
+    public function downloadFile(string $localPath, string $remotePath): bool
     {
-        return @ftp_get($this->conn, $localPath, $remotePath, FTP_BINARY);
+        $this->connect();
+        $result = @ftp_get($this->conn, $localPath, $remotePath, FTP_BINARY);
+        $this->close();
+
+        return $result;
+    }
+
+    /**
+     * @throws FtpException
+     */
+    private function connect(): void
+    {
+        if (!$this->conn = ftp_connect($this->host, null, $this->timeout)) {
+            throw new FtpException('Could not connect to FTP');
+        }
+
+        if (!@ftp_login($this->conn, $this->username, $this->password)) {
+            throw new FtpException('Could not login to FTP');
+        }
+
+        ftp_pasv($this->conn, $this->passive);
+    }
+
+    private function close(): void
+    {
+        ftp_close($this->conn);
     }
 }
